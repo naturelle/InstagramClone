9325bb9bfb838e074083c007ba5eeddd8b8e8798
Ebru E. Güngör <ebruekici@gmail.com>
2021-08-14 11:01:01 +0300

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// if(process.env.NODE_ENV==='production'){
+//     module.exports = require('./prod')
+// }else{
+//     module.exports = require('./dev')
+// }
+
+module.exports = {
+    MONGOURI: "mongodb://localhost:27017/Instagramclone",
+    JWT_SECRET: "kflkdjiooy9ıybnmbnn"
+}
